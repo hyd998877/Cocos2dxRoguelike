1089762724ff671c58e4f312bf03fcf03b31f45d
kyokomi <kyoko1220adword@gmail.com>
2014-04-13 20:40:58 +0900

    マップデータとモンスターデータを更新（４〜９層）

--- Classes/dao/MRogueMap.h
+++ Classes/dao/MRogueMap.h
@@ -74,20 +74,64 @@ namespace m_rogue_map{
         {"goldMin", Value(100)},
         {"goldMax", Value(1000)}
     };
+
+    const static ValueMap floor2_{
+        {"mobCount", Value(5)},
+        {"mobIds", Value(ValueVector{
+            // 20%
+            Value(ValueMap{ {"id", Value(0)}, {"prob", Value(2000)} }), // hazure
+            // 40%
+            Value(ValueMap{ {"id", Value(1)}, {"prob", Value(4000)} }),  // sura
+            // 20%
+            Value(ValueMap{ {"id", Value(2)}, {"prob", Value(2000)} }),  // youhei
+            // 20%
+            Value(ValueMap{ {"id", Value(3)}, {"prob", Value(2000)} }),  // youkan
+        })},
+        {"dropItemCount", Value(10)},
+        {"dropItemIds", Value(ValueVector{
+            // 60% ハズレ
+            Value(ValueMap{ {"type", Value( 0)}, {"id", Value(0)}, {"prob", Value(6000)} }), // hazure
+            //  8% 武器
+            Value(ValueMap{ {"type", Value( 1)}, {"id", Value(6)}, {"prob", Value(  10)} }), // w
+            Value(ValueMap{ {"type", Value( 1)}, {"id", Value(5)}, {"prob", Value(  40)} }), // w
+            Value(ValueMap{ {"type", Value( 1)}, {"id", Value(4)}, {"prob", Value( 100)} }), // w
+            Value(ValueMap{ {"type", Value( 1)}, {"id", Value(3)}, {"prob", Value( 100)} }), // w
+            Value(ValueMap{ {"type", Value( 1)}, {"id", Value(2)}, {"prob", Value( 150)} }), // w
+            Value(ValueMap{ {"type", Value( 1)}, {"id", Value(1)}, {"prob", Value( 400)} }), // w
+            //  8% 防具
+            Value(ValueMap{ {"type", Value( 2)}, {"id", Value(6)}, {"prob", Value(  10)} }), // a
+            Value(ValueMap{ {"type", Value( 2)}, {"id", Value(5)}, {"prob", Value(  40)} }), // a
+            Value(ValueMap{ {"type", Value( 2)}, {"id", Value(3)}, {"prob", Value( 150)} }), // a
+            Value(ValueMap{ {"type", Value( 2)}, {"id", Value(2)}, {"prob", Value( 200)} }), // a
+            Value(ValueMap{ {"type", Value( 2)}, {"id", Value(1)}, {"prob", Value( 400)} }), // a
+            // 12% ゴールド
+            Value(ValueMap{ {"type", Value(10)}, {"id", Value(7)}, {"prob", Value(1200)} }), // gold
+            // 12% 消費アイテム
+            Value(ValueMap{ {"type", Value(20)}, {"id", Value(6)}, {"prob", Value( 100)} }), // et
+            Value(ValueMap{ {"type", Value(20)}, {"id", Value(5)}, {"prob", Value( 100)} }), // et
+            Value(ValueMap{ {"type", Value(20)}, {"id", Value(4)}, {"prob", Value( 300)} }), // et
+            Value(ValueMap{ {"type", Value(20)}, {"id", Value(3)}, {"prob", Value( 100)} }), // pt
+            Value(ValueMap{ {"type", Value(20)}, {"id", Value(2)}, {"prob", Value( 100)} }), // pt
+            Value(ValueMap{ {"type", Value(20)}, {"id", Value(1)}, {"prob", Value( 500)} }), // pt
+        })},
+        {"goldMin", Value(100)},
+        {"goldMax", Value(2000)}
+    };
     
     const static ValueVector datas_{
         // 1〜3
         Value(floor1_),
         Value(floor1_),
         Value(floor1_),
-        
-        Value(floor1_),
-        Value(floor1_),
-        Value(floor1_),
-        Value(floor1_),
-        Value(floor1_),
-        Value(floor1_),
-        Value(floor1_)
+        // 4〜9
+        Value(floor2_),
+        Value(floor2_),
+        Value(floor2_),
+        Value(floor2_),
+        Value(floor2_),
+        Value(floor2_),
+        // 10
+        Value(floor2_)
     };
 }
 
