09015a8ca5e9caac3a43e606f27377ef5692b29a
kyokomi <kyoko1220adword@gmail.com>
2014-06-22 01:17:06 +0900

    scaleを整理

--- Classes/AppDelegate.cpp
+++ Classes/AppDelegate.cpp
@@ -29,6 +29,8 @@ bool AppDelegate::applicationDidFinishLaunching() {
     Director::getInstance()->setDepthTest(false);
 
 #if 1
+    float baseScale = 0.75f;
+    
     auto visibleSize = Director::getInstance()->getVisibleSize();
     int widthDiff = (int)visibleSize.width % (int)480.f;
     int heightDiff = (int)visibleSize.height % (int)320.f;
@@ -36,8 +38,8 @@ bool AppDelegate::applicationDidFinishLaunching() {
     if (widthDiff == 0 && heightDiff == 0) {
         // 800x480基準
         glview->setDesignResolutionSize(
-                                        480 * 0.75,
-                                        320 * 0.75,
+                                        480 * baseScale,
+                                        320 * baseScale,
                                         //                                    800.0f * GAME_SCALE, // 13
                                         //                                    480.0f * GAME_SCALE, //  7.8
                                         //                                     32 * 13,// 352 416   704 800
@@ -46,23 +48,23 @@ bool AppDelegate::applicationDidFinishLaunching() {
     } else {
         if (widthDiff > heightDiff) {
             // 横のほうがずれがでかいので左と右に黒い帯がでる感じ。なんとかする
-            float scale = (480.0f * 0.75) / visibleSize.width;
+            float scale = (480.0f * baseScale) / visibleSize.width;
             glview->setDesignResolutionSize(
-                                            (480 * 0.75) * (1 + scale),
-                                            (320 * 0.75),
+                                            (480 * baseScale) * (1 + scale),
+                                            (320 * baseScale),
                                             ResolutionPolicy::SHOW_ALL);
         } else {
             // 縦のほうがずれがでかいので下と上に黒い帯が出る感じ
             glview->setDesignResolutionSize(
-                                            (480 * 0.75),
-                                            (320 * 0.75),
+                                            (480 * baseScale),
+                                            (320 * baseScale),
                                             ResolutionPolicy::SHOW_ALL);
         }
     }
 #else 
     glview->setDesignResolutionSize(
-                                    (960 * 0.75),
-                                    (640 * 0.75),
+                                    (960 * baseScale),
+                                    (640 * baseScale),
                                     ResolutionPolicy::SHOW_ALL);
 #endif
     
